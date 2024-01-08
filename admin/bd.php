e8e0b5224cd96ea9e27b865b39768ad21d083768
RobBanDam <robert.mtz.a.08@gmail.com>
2024-01-08 17:32:07 -0600

    08/01/2024 Conexion a la BD

---
+++ admin/bd.php
@@ -0,0 +1,13 @@
+<?php
+
+$servidor = "localhost";
+$baseDatos = "restaurante";
+$usuario = "root";
+$contrasena = "root";
+
+//Creamos la conexión a la base de datos.
+try {
+    $conexion = new PDO("mysql:host=$servidor; dbname=$baseDatos", $usuario, $contrasena);
+} catch (Exception $error) {
+    echo $error->getMessage();
+}
